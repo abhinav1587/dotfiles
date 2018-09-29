e475bec3583f2284c44a49f9890957c11233e4e6
Abhinav <1825805+s-abhinav@users.noreply.github.com>
2018-09-28 20:22:28 -0400

    Add magit package

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,13 +5,17 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ '(package-selected-packages (quote (magit company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
